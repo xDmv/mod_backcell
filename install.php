ab2db25ac577f690b7dc5a250509f4c01b755fe8
Makushin Dmitriy <makushin.dima@gmail.com>
2017-11-10 10:19:32 +0200

    add table dannie

--- install.php
+++ install.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 $this->load->model('user/user_group');
-$this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'module/cellback');
-$this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'module/cellback');
+$this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'tool/export_import');
+$this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'tool/export_import');
