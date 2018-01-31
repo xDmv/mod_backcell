0fb3f52b7b79379f56a6449bbb550fe077d6dc25
makushin dima <makushin.dima@gmail.com>
2018-01-31 14:27:45 +0200

    up

--- mark_ups/install.php
+++ mark_ups/install.php
@@ -1,4 +1,5 @@
 <?php
-$this->load->model('user/user_group');
-$this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'module/mark_ups');
-$this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'module/mark_ups');
+  $this->load->model('user/user_group');
+  $this->model_user_user_group->addPermission($this->user->getGroupId(), 'access', 'module/mark_ups');
+  $this->model_user_user_group->addPermission($this->user->getGroupId(), 'modify', 'module/mark_ups');
+?>
